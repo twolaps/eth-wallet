973b88c444501b01a21864b8458565b429f58aef
edison <twolaps@163.com>
2026-01-04 22:01:45 +0800

    tj

--- popup.tsx
+++ popup.tsx
@@ -4,21 +4,8 @@ function IndexPopup() {
   const [data, setData] = useState("")
 
   return (
-    <div
-      style={{
-        padding: 16
-      }}>
-      <h2>
-        Welcome to your{" "}
-        <a href="https://www.plasmo.com" target="_blank">
-          Plasmo
-        </a>{" "}
-        Extension!
-      </h2>
-      <input onChange={(e) => setData(e.target.value)} value={data} />
-      <a href="https://docs.plasmo.com" target="_blank">
-        View Docs
-      </a>
+    <div>
+      <h1>Popup Page</h1>
     </div>
   )
 }
