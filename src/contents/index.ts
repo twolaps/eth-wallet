b742636a10fb25daff556d2d8d9ddb5a56c67b97
edison <twolaps@163.com>
2026-01-12 20:39:39 +0800

    tj

--- src/contents/index.ts
+++ src/contents/index.ts
@@ -34,4 +34,49 @@ const injectProvider = () => {
 	}
 }
 
+const provider = {
+	request: async (request: {method: string, params?: any[]}) => {
+		console.log("request", request.method);
+		return new Promise((resolve, rejects) => {
+			const requestId = Math.random().toString(36).substring(7);
+
+			window.addEventListener("message", (event) => {
+				if (event.data.type === "MY_WALLET_RESPONSE" && event.data.requestId === requestId) {
+					if (event.data.error) rejects(event.data.error);
+					else resolve(event.data.result);
+				}
+			}, {once: true});
+
+			window.postMessage({
+				type: "MY_WALLET_REQUEST",
+				requestId,
+				method: request.method,
+				params: request.params,
+			}, "*");
+		});
+	}
+};
+
+(window as any).ethereum = provider;
+
+const announceProvider = () => {
+	const detail = {
+		info: {
+			uuid: "my-unique-wallet-id",
+			name: "My Wallet",
+			icon: "",
+			rdns: "com.mywallet.app",
+		},
+		provider: provider,
+	};
+
+	const event = new CustomEvent("eip6963:announceProvider", {detail});
+	window.dispatchEvent(event);
+};
+
 injectProvider();
+announceProvider();
+
+window.addEventListener("eip6963:requestProvider",()=>{
+	announceProvider();
+});
